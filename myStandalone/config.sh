4ec334505f1bb1d552196e75bf9060e363af8045
AlanTai <rainman.tai@gmail.com>
2016-02-28 19:58:34 -0800

    update standalone scripts

--- myStandalone/config.sh
+++ myStandalone/config.sh
@@ -1,8 +1,11 @@
 # config. shell
-mongod --dbpath /data/db --smallfiles --keyFile /opt/keyfile/mongodb-keyfile
-
 # set authentication
-mongo admin --eval "db.createUser({user:'siteUserAdmin',pwd:'standaloneadmin',roles:[{role:'userAdminAnyDatabase',db:'admin'}]}); db.auth('siteUserAdmin', 'standaloneadmin'); db.createUser({user:'siteRootAdmin',pwd:'standaloneadmin',roles:[{role:'root',db:'admin'}]}); db.auth('siteRootAdmin', 'standaloneadmin'); db.auth('siteRootAdmin', 'standaloneadmin'); db.createUser({user:'test_user',pwd:'standalonetestuser',roles:[{role:'readWrite',db:'test'}]});"
+echo "use admin" >> authentication.js
+echo "db.createUser({user:'siteUserAdmin',pwd:'standaloneadmin',roles:[{role:'userAdminAnyDatabase',db:'admin'}]})" >> authentication.js 
+echo "db.auth('siteUserAdmin', 'standaloneadmin')" >> authentication.js
+echo "db.createUser({user:'siteRootAdmin',pwd:'standaloneadmin',roles:[{role:'root',db:'admin'}]})" >> authentication.js
+echo "db.auth('siteRootAdmin', 'standaloneadmin'); db.auth('siteRootAdmin', 'standaloneadmin')" >> authentication.js
+echo "db.createUser({user:'test_user',pwd:'standalonetestuser',roles:[{role:'readWrite',db:'test'}]})" >> authentication.js
 
 # restart supervisord
 /etc/init.d/supervisor restart
--- myStandalone/configuration.md
+++ myStandalone/configuration.md
@@ -14,4 +14,12 @@ nano Dockerfile
 
 NOTE: You can create your own Dockerfile or clone this repo. and use the existing Dockerfile to create a standalone container
 
-**3. Create Images**
+**3. Build Images, run container, and set authentication **
+
+docker build -t alantai/my_standalone_mongodb .
+
+docker run --name mongo_standalone -p 27017:27017 -d alantai/my_standalone_mongodb
+
+docker exec mongo_standalone bash ./config.sh
+
+docker exec -it mongo_standalone bash
