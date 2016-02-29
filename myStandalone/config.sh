642b4056678f66ba43d6bd7901fab2e543e210b9
AlanTai <rainman.tai@gmail.com>
2016-02-28 20:50:03 -0800

    update scripts

--- myStandalone/config.sh
+++ myStandalone/config.sh
@@ -1,14 +1,16 @@
 # config. shell
 # set authentication
-echo "use admin" >> authentication.sh
-echo "db.createUser({user:'siteUserAdmin',pwd:'standaloneadmin',roles:[{role:'userAdminAnyDatabase',db:'admin'}]})" >> authentication.sh
-echo "db.auth('siteUserAdmin', 'standaloneadmin')" >> authentication.sh
-echo "db.createUser({user:'siteRootAdmin',pwd:'standaloneadmin',roles:[{role:'root',db:'admin'}]})" >> authentication.sh
-echo "db.auth('siteRootAdmin', 'standaloneadmin'); db.auth('siteRootAdmin', 'standaloneadmin')" >> authentication.sh
-echo "db.createUser({user:'test_user',pwd:'standalonetestuser',roles:[{role:'readWrite',db:'test'}]})" >> authentication.sh
+echo "use admin" >> authentication.js
+echo "db.createUser({user:'siteUserAdmin',pwd:'standaloneadmin',roles:[{role:'userAdminAnyDatabase',db:'admin'}]})" >> authentication.js
+echo "db.auth('siteUserAdmin', 'standaloneadmin')" >> authentication.js
+echo "db.createUser({user:'siteRootAdmin',pwd:'standaloneadmin',roles:[{role:'root',db:'admin'}]})" >> authentication.js
+echo "db.auth('siteRootAdmin', 'standaloneadmin')" >> authentication.js
+echo "use test" >> authentication.js
+echo "db.createUser({user:'test_user',pwd:'standalonetestuser',roles:[{role:'readWrite',db:'test'}]})" >> authentication.js
+echo "db.auth('test_user', 'standalonetestuser')" >> authentication.js
 
 # run script
-mongo < authentication.sh
+mongo < authentication.js
 
 # restart supervisord
 /etc/init.d/supervisor restart
