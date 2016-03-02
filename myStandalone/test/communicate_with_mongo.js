63d132ce5c7afd8fdb23a86aad34d34cdec4ebda
AlanTai <rainman.tai@gmail.com>
2016-03-02 15:48:26 -0800

    add nodejs scritps

--- myStandalone/test/communicate_with_mongo.js
+++ myStandalone/test/communicate_with_mongo.js
@@ -1,1 +1,16 @@
 /**/
+/* mongodb */
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
+    users_collection;
+ 
+// Connection URL 
+var url = 'mongodb://test_user:standalonetestuser@45.33.61.89:27025/test';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to the test server");
+  users_collection  = db.collection('users');
+  db.close();
+});
+/* end of mongodb */
