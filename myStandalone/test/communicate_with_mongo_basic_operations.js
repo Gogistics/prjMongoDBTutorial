a085ba17efbe09a3de01c16a3f64fccad91f35f7
AlanTai <rainman.tai@gmail.com>
2016-03-03 10:20:26 -0800

    add mongo operations

--- myStandalone/test/communicate_with_mongo_basic_operations.js
+++ myStandalone/test/communicate_with_mongo_basic_operations.js
@@ -1,7 +1,10 @@
-/**/
+/* basic operation */
 /* mongodb */
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert'),
+    test_docs = require('jsonfile').readFileSync('./test_docs.json'),
+    my_close_db_action,
+    my_db,
     users_collection;
  
 // Connection URL 
@@ -10,22 +13,29 @@ var url = 'mongodb://test_user:standalonetestuser@45.33.61.89:27025/test';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to the test server");
+  my_db = db;
   users_collection  = db.collection('users');
 
+  if(test_docs){
+    for(var ith = 0; ith < test_docs.length; ith++){
+      console.log(test_docs[ith]);
+    }
+  }
+
   // inser tnew doc
-  insert_if_not_exist('test@gmail.com', {email: 'test@gmail.com', first_name: 'Alan', last_name: 'Tai', phone: '(408) 565-5133'});
+  insert_if_not_exist('eric@hotmail.com', {email: 'eric@hotmail.com', first_name: 'Eric', last_name: 'Kent', phone: '(610) 113-2311', birth_year: '1977'});
 
   // update doc
-  update_doc('test@gmail.com');
+  update_doc('mary@gmail.com');
 
   // query
-  find_docs({email:'test@gmail.com'});
+  find_docs({});
 
   // close db once testing id done
-  // db.close();
+  close_db(db);
 });
 /* end of mongodb */
-
+var is_insert_if_not_exist_done = false;
 function insert_if_not_exist(arg_email, arg_user_info){
   users_collection.update({email: arg_email}, arg_user_info, {upsert: true}, function(err, doc){
     // return status
@@ -35,19 +45,25 @@ function insert_if_not_exist(arg_email, arg_user_info){
     }else{
       console.log('failed to insert new doc');
     }
+    is_insert_if_not_exist_done = true;
   });
 }
 
+// update docs
+var is_update_doc_done = false;
 function update_doc(arg_email){
-  users_collection.updateOne({ email: arg_email }, { $set: { gender : 'M' } }, function(err, doc) {
+  users_collection.updateOne({ email: arg_email }, { $set: { gender : 'F' } }, function(err, doc) {
     if(!err){
       console.log(doc);
     }else{
       console.log(err);
     }
+    is_update_doc_done = true;
   });  
 }
 
+// find docs
+var is_find_docs = false;
 function find_docs(arg_query){
   // get users
   users_collection.find(arg_query).toArray(function(err, docs){
@@ -56,5 +72,19 @@ function find_docs(arg_query){
     }else{
       console.log(err);
     }
+    is_find_docs = true;
   });
+}
+
+// close db if all operations are done
+function close_db(arg_db){
+  if( is_insert_if_not_exist_done &&
+      is_update_doc_done &&
+      is_find_docs){
+    // clear timeout action
+    if(my_close_db_action) clearTimeout(my_close_db_action);
+    if(arg_db) arg_db.close();
+  }else{
+    my_close_db_action = setTimeout(close_db.bind(this, arg_db), 2000);
+  }
 }
