37cf53555b7a59f0f072c65ce07c3c4c9a58f33a
AlanTai <rainman.tai@gmail.com>
2016-03-12 14:57:27 -0800

    update dir

--- myMongoGraphQLServer/configuration.md
+++
@@ -1,5 +0,0 @@
-###Configuration of GraphQL end-point server with MongoDB
-
-Ref. Links:
-
-[GraphQL Server with Node.js and MongoDB](https://github.com/sitepoint-editors/graphql-nodejs)
--- myRaspi/myStandalone/config.sh
+++ myRaspi/myStandalone/config.sh
@@ -1,12 +1,12 @@
 # config. shell
 # set authentication
 echo "use admin" >> authentication.js
-echo "db.createUser({user:'siteUserAdmin',pwd:'standaloneadmin',roles:[{role:'userAdminAnyDatabase',db:'admin'}]})" >> authentication.js
+echo "db.addUser({user:'siteUserAdmin',pwd:'standaloneadmin',userSource:'admin',roles:['userAdminAnyDatabase']})" >> authentication.js
 echo "db.auth('siteUserAdmin', 'standaloneadmin')" >> authentication.js
-echo "db.createUser({user:'siteRootAdmin',pwd:'standaloneadmin',roles:[{role:'root',db:'admin'}]})" >> authentication.js
+echo "db.addUser({user:'siteRootAdmin',pwd:'standaloneadmin',userSource:'admin',roles:['userAdmin']})" >> authentication.js
 echo "db.auth('siteRootAdmin', 'standaloneadmin')" >> authentication.js
 echo "use test" >> authentication.js
-echo "db.createUser({user:'test_user',pwd:'standalonetestuser',roles:[{role:'readWrite',db:'test'}]})" >> authentication.js
+echo "db.addUser({user:'test_user',pwd:'standalonetestuser',userSource:'test',roles:['readWrite']})" >> authentication.js
 echo "db.auth('test_user', 'standalonetestuser')" >> authentication.js
 
 # run script
