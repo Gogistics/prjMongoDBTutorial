2f9fb1c28b8432ccf13b3ba3080870298157e8af
AlanTai <rainman.tai@gmail.com>
2016-04-09 10:54:02 -0700

    update basic operation

--- myStandalone/test/communicate_with_mongo_basic_operations.js
+++ myStandalone/test/communicate_with_mongo_basic_operations.js
@@ -16,25 +16,29 @@ MongoClient.connect(url, function(err, db) {
   my_db = db;
   users_collection  = db.collection('users');
 
-  if(test_docs){
-    for(var ith = 0; ith < test_docs.length; ith++){
-      console.log(test_docs[ith]);
-    }
-  }
+  // insert docs for testing
+  // if(test_docs){
+  //   for(var ith = 0; ith < test_docs.length; ith++){
+  //     console.log(test_docs[ith]);
+  //   }
+  // }
 
   // inser tnew doc
   // insert_if_not_exist('mary@hotmail.com', {email: 'mary@hotmail.com', first_name: 'Mary', last_name: 'Lisan', phone: '(650) 456-2311', birth_year: '1977', annual_salary: 87200});
 
   // update doc
-  update_doc( { first_name: "Eric" }, { annual_salary: 71200 }, 'set', false);
+  // update_doc( { first_name: "Eric" }, { annual_salary: 71200 }, 'set', false);
+
+  // findAndModify
+  find_and_modify_doc({email: 'cindy@gmail.com'}, {gender: "femmale"});
 
   // query
-  find_docs({});
+  // find_docs({});
 
   // close db once testing id done
-  close_db(db);
+  // close_db(db);
 });
-/* end of mongodb */
+
 var is_insert_if_not_exist_done = false;
 function insert_if_not_exist(arg_email, arg_user_info){
   users_collection.update({email: arg_email}, arg_user_info, {upsert: true}, function(err, doc){
@@ -70,6 +74,24 @@ function update_doc(arg_query, arg_updated_doc, arg_operator, arg_upsert){
     }
     is_update_doc_done = true;
   });  
+}
+
+// find and modify
+function find_and_modify_doc(arg_query, arg_updated_doc){
+  users_collection.findAndModify(
+    arg_query,
+    [['_id','asc']],
+    { $set: arg_updated_doc },
+    { update: true },
+    function(err, doc){
+      if(!err){
+        console.log(doc);
+      }else{
+        console.log(err);
+      }
+      my_db.close();
+    }
+  );
 }
 
 // find docs
